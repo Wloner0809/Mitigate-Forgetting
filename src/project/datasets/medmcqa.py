6da6ca46e134619b5dd9573687cd0f3f61f56411
Wloner0809 <wloner@mail.ustc.edu.cn>
2024-04-01 20:27:10 +0800

    fix(dataset): fix some bugs and typos

--- src/project/datasets/medmcqa.py
+++ src/project/datasets/medmcqa.py
@@ -1,7 +1,51 @@
 from torch.utils.data import Dataset
 from datasets import load_dataset
 from transformers import AutoTokenizer
-from utils.prompter import Prompter
+import json
+import os.path as osp
+from typing import Union
+
+
+class Prompter(object):
+    __slots__ = ("template", "_verbose")
+
+    def __init__(self, template_name: str = "", verbose: bool = False):
+        self._verbose = verbose
+        if not template_name:
+            # Enforce the default here, so the constructor can be called with '' and will not break.
+            template_name = "alpaca"
+        file_name = osp.join("src/project/datasets/", f"{template_name}.json")
+        if not osp.exists(file_name):
+            raise ValueError(f"Can't read {file_name}")
+        with open(file_name) as fp:
+            self.template = json.load(fp)
+        if self._verbose:
+            print(
+                f"Using prompt template {template_name}: {self.template['description']}"
+            )
+
+    def generate_prompt(
+        self,
+        instruction: str,
+        input: Union[None, str] = None,
+        label: Union[None, str] = None,
+    ) -> str:
+        # returns the full prompt from instruction and optional input
+        # if a label (=response, =output) is provided, it's also appended.
+        if input:
+            res = self.template["prompt_input"].format(
+                instruction=instruction, input=input
+            )
+        else:
+            res = self.template["prompt_no_input"].format(instruction=instruction)
+        if label:
+            res = f"{res}{label}"
+        if self._verbose:
+            print(res)
+        return res
+
+    def get_response(self, output: str) -> str:
+        return output.split(self.template["response_split"])[1].strip()
 
 
 class MedmcqaDataset(Dataset):
@@ -55,7 +99,8 @@ class MedmcqaDataset(Dataset):
         )
 
         tokenizer = AutoTokenizer.from_pretrained(self.tokenizer_path)
-        tokenizer.pad_token_id = 0
+        tokenizer.pad_token_id = tokenizer.eos_token_id
+        tokenizer.pad_token = tokenizer.eos_token
         tokenizer.padding_side = "left"
 
         all_data = all_data.map(
@@ -83,7 +128,7 @@ class MedmcqaDataset(Dataset):
                 prompt,
                 truncation=True,
                 max_length=512,
-                padding=False,
+                padding="max_length",
                 return_tensors=None,
             )
             if (
