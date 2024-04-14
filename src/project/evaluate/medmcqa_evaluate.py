d6860f25f31bd1711759bc4e64018d4154f16611
Wloner0809 <wloner@mail.ustc.edu.cn>
2024-04-14 19:13:31 +0800

    feat(medmcqa evaluate): plot the distribution of options

--- src/project/evaluate/medmcqa_evaluate.py
+++ src/project/evaluate/medmcqa_evaluate.py
@@ -1,5 +1,6 @@
 import re
 import json
+from matplotlib import pyplot as plt
 
 
 def preprocess(sentence):
@@ -11,25 +12,87 @@ def preprocess(sentence):
     return sentence
 
 
-def evaluate_medmcqa(output, ground_truth):
+def evaluate_medmcqa(output, ground_truth, path):
     total = len(ground_truth)
     acc = 0
+    A, B, C, D = 0, 0, 0, 0
+    A_truth, B_truth, C_truth, D_truth = 0, 0, 0, 0
     for i in range(len(output)):
         output[i] = preprocess(output[i])
         ground_truth[i] = preprocess(ground_truth[i])
         if output[i] == ground_truth[i]:
             acc += 1
+        if output[i] == "A":
+            A += 1
+        elif output[i] == "B":
+            B += 1
+        elif output[i] == "C":
+            C += 1
+        elif output[i] == "D":
+            D += 1
+        if ground_truth[i] == "A":
+            A_truth += 1
+        elif ground_truth[i] == "B":
+            B_truth += 1
+        elif ground_truth[i] == "C":
+            C_truth += 1
+        elif ground_truth[i] == "D":
+            D_truth += 1
+    plt.figure(figsize=(6, 4))
+    plt.title("MedMCQA Evaluation", loc="center", fontweight="bold")
+    plt.xlabel("Options", loc="center", fontweight="bold")
+    plt.ylabel("Count", loc="center", fontweight="bold")
+    plt.bar(
+        [i for i in range(4)],
+        [A, B, C, D],
+        label="Output",
+        width=0.2,
+        color="#f9766e",
+        edgecolor="k",
+        zorder=2,
+    )
+    plt.bar(
+        [i + 0.2 for i in range(4)],
+        [A_truth, B_truth, C_truth, D_truth],
+        label="Ground Truth",
+        width=0.2,
+        color="#00bfc4",
+        edgecolor="k",
+        zorder=2,
+        hatch="/",
+    )
+    plt.xticks([i + 0.1 for i in range(4)], ["A", "B", "C", "D"], fontweight="bold")
+    plt.yticks(fontweight="bold")
+    plt.legend(frameon=False, loc="best")
+    for a, b, i in zip([A, B, C, D], [A_truth, B_truth, C_truth, D_truth], range(4)):
+        plt.text(i, a, str(a), ha="center", va="bottom", fontsize=10, fontweight="bold")
+        plt.text(
+            i + 0.2, b, str(b), ha="center", va="bottom", fontsize=10, fontweight="bold"
+        )
+    plt.grid(ls="--", alpha=0.8)
+    plt.gca().spines["top"].set_visible(False)
+    plt.gca().spines["right"].set_visible(False)
+    plt.gca().spines["left"].set_linestyle("-")
+    plt.gca().spines["left"].set_linewidth(2.5)
+    plt.gca().spines["bottom"].set_linestyle("-")
+    plt.gca().spines["bottom"].set_linewidth(2.5)
+    plt.tight_layout()
+    plt.show()
+    plt.savefig(path)
+    plt.clf()
     return acc / total
 
 
 if __name__ == "__main__":
     with open(
-        "work_dirs/lit_llama_lora_inference/medmcqa.json", "r", encoding="utf-8"
+        "work_dirs/lit_llama_lora_inference/medmcqa_freeze.json", "r", encoding="utf-8"
     ) as f:
         result = json.load(f)
     answer = result["answer"]
     output = result["output"]
-    accuracy = evaluate_medmcqa(output, answer)
+    accuracy = evaluate_medmcqa(
+        output, answer, "work_dirs/lit_llama_lora_inference/medmcqa_freeze.png"
+    )
     print(f"LoRA finetuned Accuracy: {accuracy}")
     with open(
         "work_dirs/lit_llama_lora_inference/medmcqa_baseline.json",
@@ -39,5 +102,7 @@ if __name__ == "__main__":
         result = json.load(f)
     answer = result["answer"]
     output = result["output"]
-    accuracy = evaluate_medmcqa(output, answer)
+    accuracy = evaluate_medmcqa(
+        output, answer, "work_dirs/lit_llama_lora_inference/medmcqa_baseline.png"
+    )
     print(f"Baseline Accuracy: {accuracy}")
